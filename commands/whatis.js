186e29f0744a54f6d45a41f0704c5e13116f4594
ItachiLeDev <33499834+karim-uchiwa@users.noreply.github.com>
2018-02-19 22:05:00 +0100

    Update whatis.js

--- commands/whatis.js
+++ commands/whatis.js
@@ -4,8 +4,8 @@ const Command = require('./command')
 module.exports = class Whatis extends Command {
 
 	static match(message) {
-		let msg = message.content.toLowerCase();
-		return msg.startsWith('d?whatis')
+		let content = message.content.toLowerCase();
+		return content.startsWith('d?whatis')
 	}
 
 	static action(message) {
