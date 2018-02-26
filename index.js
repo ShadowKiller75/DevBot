2d7854db199fe2b937e08fdcadf07ff12624c4fe
K A R I M <33499834+karim-vapor@users.noreply.github.com>
2018-02-26 11:05:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.on('message', function(message) {
 
 		var embed = new Discord.RichEmbed()
 		.setColor("#226666")
-		.addField("Tous les roles disponibles", "-Développeur ->  d?role -dev\n-Youtubeur ->      d?role -youtubeur\n-Hacker ->           d?role -hacker\n-Graphiste ->     d?role -graphiste\n-Rédacteur ->    d?role -redacteur\n-Front-end ->      d?role -frontend\n-Back-end ->      d?role -backend\n-Full-stack ->      d?role -fullstack")
+		.addField("Tous les roles disponibles", "-Développeur ->  d?role -dev\n-Youtubeur ->      d?role -youtubeur\n-Hacker ->           d?role -hacker\n-Graphiste ->     d?role -graphiste\n-Rédacteur ->    d?role -redacteur\n-Front-end ->      d?role -frontend\n-Back-end ->      d?role -backend\n-Full-stack ->      d?role -fullstack\n-Gamer      d?role -gamer")
 		message.channel.send(embed).catch(console.error)
 
 	} else if(message.content.startsWith('d?game')) {
