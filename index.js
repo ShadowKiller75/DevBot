c4807943966832068f5e23a5a2a6ffb38b34ed06
K A R I M <33499834+karim-vapor@users.noreply.github.com>
2018-02-26 22:14:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,11 @@ bot.on('message', function(message) {
 		
 	} else if(message.content === 'd?online_members') {
 		  
-		message.channel.send(bot.users.length + ' membres en lignes');
+		var Count;
+		    for(Count in Bot.users.array()){
+		       var User = bot.users.array()[Count];
+		       message.channel.send(User + ' membres en lignes');
+		    }
 	}
  	
 });
